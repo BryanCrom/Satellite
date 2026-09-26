74b66caa08277b5c0ab6dc6cb8a44cd18917cee7
Bryan <BryanCrombach@gmail.com>
2026-09-26 21:06:25 +1200

    feat(run.ps1, setup.ps1): create powershell scripts to setup and run the satellitle project with ease.

--- run.ps1
+++ run.ps1
@@ -1,10 +1,3 @@
-$natsServerProcess = Start-Process -FilePath "nats-server" -PassThru
-Start-Sleep -Seconds 1
-
-try{
-    ./setup.ps1
-    uv run -m satellite
-}
-finally{
-    Stop-Process -Id $natsServerProcess.Id -Force -ErrorAction SilentlyContinue
-}
+Push-Location $PSScriptRoot
+./setup.ps1
+uv run -m satellite
--- setup.ps1
+++ setup.ps1
@@ -1,2 +1,3 @@
+Push-Location $PSScriptRoot
 .venv/Scripts/activate.ps1
 uv sync
